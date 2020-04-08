b7cb3e94661dfe848417e97babc327d9db869865
Herko Baarslag <herkobaarslag@iMac-van-Herko.local>
2020-04-08 10:45:12 +0200

    APE-20 flex blocks in seperate files, blocks called via get_template_part()

--- acf-flex-content-loop.php
+++ acf-flex-content-loop.php
@@ -6,26 +6,26 @@
 		// loop through the rows of data
 		while ( have_rows('flex') ) : the_row();
 			
-			$row_layouts = array(
-				0 =>	'flex-text',
-				1 =>	'flex-image',
-				2 =>	'flex-gallery',
-				3 =>	'flex-slider',
-				4 =>	'flex-youtube',
-				5 =>	'flex-cta',
-				6 =>	'flex-usp',
-				7 =>	'flex-html',
-				8 =>	'flex-php-include',
-				9 =>	'flex-table',
-				10=>	'flex-display-child-pages',
-				11=>	'flex-form',
-				12=>	'flex-mod-occasions',
-				13=>	'flex-maps'
-			);
+			// $row_layouts = array(
+			// 	0 =>	'flex-text',
+			// 	1 =>	'flex-image',
+			// 	2 =>	'flex-gallery',
+			// 	3 =>	'flex-slider',
+			// 	4 =>	'flex-youtube',
+			// 	5 =>	'flex-cta',
+			// 	6 =>	'flex-usp',
+			// 	7 =>	'flex-html',
+			// 	8 =>	'flex-php-include',
+			// 	9 =>	'flex-table',
+			// 	10=>	'flex-display-child-pages',
+			// 	11=>	'flex-form',
+			// 	12=>	'flex-mod-occasions',
+			// 	13=>	'flex-maps'
+			// );
 			
 			$row_layout = get_row_layout();
 		
-			if( in_array($row_layout, $row_layouts) && validateFlexItem(get_sub_field('flex-options')) ) :
+			if( validateFlexItem(get_sub_field('flex-options')) ) :
 		
 				get_template_part('/blocks/' . $row_layout);
 		
